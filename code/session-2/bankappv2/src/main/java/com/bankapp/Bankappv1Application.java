89d5eebc54837ad1a2ae402d6c6450654d414cd2
rgupta00 <rgupta.mtech@gmail.com>
2024-09-26 12:57:51 +0530

    new project is added

---
+++ code/session-2/bankappv2/src/main/java/com/bankapp/Bankappv1Application.java
@@ -0,0 +1,13 @@
+package com.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bankappv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bankappv1Application.class, args);
+	}
+
+}
